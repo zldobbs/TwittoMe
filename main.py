9b8e656672d25b2b83f383d4fb5f010ff78f1d55
Zach Dobbs <dobbszach@gmail.com>
2017-11-30 19:41:20 +0000

    Update main documentation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# TwittoMe
+# A final project for CS4830 developed using Flask. Works with the
+# Twitter and GroupMe APIs to create a cohesive bot that polls for new
+# tweets from user specified accounts.
+# Developed by Zachary Dobbs and Austin Sizemore
+
 # --------------- INITIALIZATION AND CONFIG ----
 # imports and initiatilization
 from flask import Flask, request, render_template, url_for
@@ -8,90 +14,112 @@ application = Flask(__name__)
 auth = tweepy.OAuthHandler(config.twit['consumer_key'], config.twit['consumer_secret'])
 auth.set_access_token(config.twit['access_token_key'], config.twit['access_token_secret'])
 
+# initialize the tweepy api
 api = tweepy.API(auth)
 
 # ------------- POLLING THREAD -----------------
 # create a thread that will run in the background
 # this thread will poll the user's twitter app every 15 seconds
 # to check if a new tweet has been posted
+# twitter's API restricts the retrieval of data to 10 second intervals
 class PollingThread(object):
+    # establish thread details
     def __init__(self, interval=15):
-        # establish thread details
+        # the interval is the time between polls to the api
         self.interval = interval
-        # create an option to change the followed user via the GroupMe bot -- done
-        # make the username an array, call the poll on each username
+        # users will contain the accounts currently being followed
+        # defaults to Donald Trump at the start of the server
+        # the top parameter specifies the ID of the user's most recent tweet
         self.users = [ {
             'username'  : '@realDonaldTrump',
             'top'       : 0 } ]
+        # start the thread as a daemon so it does not interrupt normal functionality
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
         thread.start()
 
+    # method will follow add a new user to the current follow list
     def add_tweeter(self, name):
-        # add a username to the users
+        # limit the amount of users being followed at one time to 5 users
         if len(self.users) >= 5:
             send_message("A maximum of 5 users may be followed at once! Type #twit unfollow <name> to open a spot")
             return 0
+        # keep the data consistent by enforcing an @ character in each username
         if '@' not in name:
             name = '@' + name
+        # check if the user is already being followed, don't want a duplicate
         for tweeter in self.users:
             if tweeter['username'].lower() == name.lower():
                 send_message("Follow failed! User is already being followed!")
                 return 0
+        # must try to follow the user
         try:
             api.user_timeline(screen_name = name)
             tweeter = {'username': name, 'top': 0}
             self.users.append(tweeter)
+        # if it fails, it means that the username sent was either not real or a private account
         except Exception as e:
             print(e)
             send_message("Follow failed! Please make sure the account name is correct and not private!")
 
+    # method will remove a username from the list of followed users
     def remove_tweeter(self, name):
-        # remove username from users
+        # check for an @ character being sent, must maintain data consistency
         if '@' not in name:
             name = '@' + name
+        # iterate through the current users, if the user is found then it will be removed
         for index, tweeter in enumerate(self.users):
             if tweeter['username'].lower() == name.lower():
                 del self.users[index]
                 send_message("Successfully unfollowed " + name + "!")
                 return 0
+        # if the method doesn't return above, then the user specified was not found
         send_message("User specified is not currently being followed!")
 
+    # method will print all accounts currently being followed
     def list(self):
-        # list all usernames in usernames
+        # check if noone is being followed
         if len(self.users) == 0:
             msg = "Noone is being followed! Type #twit follow <user> to start following!"
+        # if it is not empty, iterate through each user and print their name
         else:
             msg = "Users currently being followed: "
             for tweeter in self.users:
                 msg = msg + tweeter['username'] + ' '
         send_message(msg)
 
+    # method that will poll for a new tweet for a given user
     def query_user(self, tweeter):
-        # check for the new tweet
+        # if top is 0, then no tweets for this user have been evaluated yet
         if tweeter['top'] == 0:
-            # pull the user's most recent tweet to reference against
+            # status will contain the tweet infromation retrieved from the API
             status = api.user_timeline(screen_name = tweeter['username'], count = 1, tweet_mode = 'extended')
+            # set top to the id of the tweet. this id increments with each new tweet
             tweeter['top'] = status[0].id
             print("Init: " + html.unescape(status[0].full_text))
+            # maintain data consistency by checking for the @ character
             if '@' not in tweeter['username']:
-                # account for both methods some person may input a username during follow
                 send_message("Started following @" + tweeter['username'] + "!")
             else:
                 send_message("Started following " + tweeter['username'] + "!")
+            # send the tweet retrieved
             send_message(status[0].user.name + ": " + html.unescape(status[0].full_text))
+        # if top is not 0, then some tweet has already been referenced
         else:
             status = api.user_timeline(screen_name = tweeter['username'], count = 1, tweet_mode = 'extended')
             curr = status[0]
-            # check if the retrieved tweet is newer than the current recent
+            # check if the retrieved tweet is newer than current top tweet
             if curr.id > tweeter['top']:
               print("New tweet! -> " + html.unescape(curr.full_text))
               send_message(curr.user.name + ": " + html.unescape(curr.full_text))
+              # set the new top to the current tweet
               tweeter['top'] = curr.id
 
+    # method for the looping that will run on the daemon thread
     def run(self):
-        # where the actual polling will take place
+        # infinite loop, this process should run in the backround continuously
         while True:
+            # for each user being followed, poll for their most recent tweet
             for tweeter in self.users:
                 self.query_user(tweeter)
             # wait 15 seconds before checking again
@@ -102,71 +130,67 @@ class PollingThread(object):
 # start the polling!
 poll = PollingThread()
 
-# -------------- STREAM -----------------------
-# create a stream to listen to tweets coming in
-# class MyStreamListener(tweepy.StreamListener):
-# 	def on_status(self, status):
-# 		print(status.text)
-
-# initialize the stream, this isn't really doing anything at the moment
-# myStreamListener = MyStreamListener()
-# myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
-# myStream.filter(follow=['espn', async=True)
-
-# --------------- GROUPME BOT -----------------
+# --------------- GROUPME BOT ------------------
+# sends the specified message to the GroupMe chat
 def send_message(msg):
-    # POST to the GroupMe chat
     print('Attempting to send: ' + msg)
+    # uses a post request to send to the chat
+    # the bot's ID is stored in an external config file
     r = requests.post('https://api.groupme.com/v3/bots/post', data = {"text" : msg, "bot_id" : config.meBot['bot_id']})
 
-# --------------- ROUTES ----------------------
-# route for display page of the applicaiton
-# not too much functionality needed here
+# --------------- ROUTES -----------------------
+# handle the display page of the applicaiton
 @application.route("/", methods=['GET'])
 def index():
-	user = api.get_user('twitter')
-	return render_template('index.html')
+	return render_template('home.html')
 
-# receive messages from the GroupMe Bot
+# handle the callback of messages sent from the group chat
 @application.route("/msg", methods=['POST'])
 def handle():
-    # do something
+    # get the message sent in json format
     data = request.get_json()
+    # make sure that the the message sent is from an actual user and not the bot
+    # #twit will denote the signal for a bot command
     if data['name'] != 'TwittoBot' and '#twit' in data['text']:
-        # create a switch..case under @twit to handle the various commands
-        # turns out python doesn't have switch..case statements..
+        # parse the received message for a valid command
         tokens = data['text'].split()
+        # default to the error message, will change if a valid command is found
         msg = "Sorry, I didn't recognize that command. Try typing '#twit help' to list available commands"
+        # commands
         if tokens[1]:
             command = tokens[1]
+            # help will display some basic information on the application
             if command == 'help':
-                # display help information
                 msg = "Follow your favorite accounts to receive realtime updates on new tweets! Some available commands: "
-                msg = msg + "follow <user>, unfollow <user>, list" 
+                msg = msg + "follow <user>, unfollow <user>, list"
+            # follow will add a user to the follow list
             if command == 'follow':
-                # follow a new user
+                # check if a username was specified
                 if not tokens[2]:
                     msg = "You must specify a valid public user to follow!"
+                # try to follow the specified user
                 else:
                     msg = ""
                     poll.add_tweeter(tokens[2])
+            # list will display all users being followed
             if command == 'list':
-                # list all users currently being followed
                 msg = ""
                 poll.list()
+            # unfollow will remove a user from the follow list
             if command == 'unfollow':
-                # unfollow a user currently being followed
+                # check if a username was specified
                 if not tokens[2]:
                     msg = "You must specify a user to unfollow!"
+                # try to unfollow the specified user
                 else:
                     msg = ""
                     poll.remove_tweeter(tokens[2])
+        # send a message to the chat, will either be an error or success response
         send_message(msg)
         print("Original message sent -> " + data['text'])
     return "Righteous!"
 
-# --------------- SERVER START ------------------
+# --------------- SERVER START -----------------
 if __name__ == "__main__":
 	# 8080 port is allowed inbound on AWS
-	# try 0.0.0.0 as host for now, if it doesn't work switch to the ip of AWS
 	application.run(host='0.0.0.0', port='8080')
